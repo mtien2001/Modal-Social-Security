aa4aceeac31259d2b20611f46d5c8a3563261167
mtien <thuy@react-plus.com>
2023-01-16 16:08:15 +0700

    update

--- src/repositories/modal_social_security_repository/convert.tsx
+++ src/repositories/modal_social_security_repository/convert.tsx
@@ -4,28 +4,28 @@ export const convertData = (data: any) => {
     return {
         whenHeDied: {
             isDisable: data.when_he_hied.isDisable,
-            budget: getLabels(data?.whenHeDied.budget),
-            money: data.whenHeDied.money
+            budget: getLabels(data?.when_he_hied.budget),
+            money: data.when_he_hied.money
         },
         whenMedicalExpensesBecomeHigh: {
             isDisable: data.when_medical_expenses_become_high.isDisable,
-            budget: getLabels(data?.whenMedicalExpensesBecomeHigh.budget),
-            money: data.whenMedicalExpensesBecomeHigh.money
+            budget: getLabels(data?.when_medical_expenses_become_high.budget),
+            money: data.when_medical_expenses_become_high.money
         },
         whenYouCanNotWork: {
             isDisable: data.when_you_can_not_work.isDisable,
-            budget: getLabels(data?.whenYouCanNotWork.budget),
-            money: data.whenYouCanNotWork.money,
+            budget: getLabels(data?.when_you_can_not_work.budget),
+            money: data.when_you_can_not_work.money,
         },
         whenInTrouble: {
             isDisable: data.when_in_trouble.isDisable,
-            budget: getLabels(data?.whenInTrouble.budget),
-            money: data.whenInTrouble.money
+            budget: getLabels(data?.when_in_trouble.budget),
+            money: data.when_in_trouble.money
         },
         whenChildBorn: {
             isDisable: data.when_child_born.isDisable,
-            budget: getLabels(data?.whenChildBorn.budget),
-            money: data.whenChildBorn.money
+            budget: getLabels(data?.when_child_born.budget),
+            money: data.when_child_born.money
         }
     }
 }
