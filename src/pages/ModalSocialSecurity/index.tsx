e286706851522b9d7825d259a30efbbcc24328a6
mtien <thuy@react-plus.com>
2023-01-18 16:55:28 +0700

    update

--- src/pages/ModalSocialSecurity/index.tsx
+++ src/pages/ModalSocialSecurity/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   ModalStyled as Modal,
   ModalHeader,
@@ -19,12 +19,6 @@ const Component = (
   const onClose = () => {
     setIsOpen(!isOpen);
   };
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsOpen(!isOpen);
-    }, 3000);
-  }, []);
 
   return (
     <Modal
